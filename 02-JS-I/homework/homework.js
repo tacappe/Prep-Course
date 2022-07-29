3ff4f5a8907319b982202b118405e942df1fab1a
Tomas <37308737+tacappe@users.noreply.github.com>
2022-07-29 12:16:25 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -243,7 +243,7 @@ function esVocal(letra){
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
     return 'Es vocal'
   }
-  if else ( letra.length !== 1) {
+  else ( letra.length !== 1) {
     return 'Dato incorrecto'
   }
 return 'Dato incorrecto'
